16d37f263fd4a566d5293c466b2e66203a0a056f
luizengdev <luizengdev@gmail.com>
2024-01-16 17:54:45 -0300

    feat: add router file task

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import router from "./src/routes/task.routes.js";
 import connectToDatabase from "./src/database/mongoose.database.js";
-import TaskModel from "./src/models/task.model.js";
 
 dotenv.config();
 const app = express();
@@ -10,56 +10,7 @@ app.use(express.json());
 
 connectToDatabase();
 
-app.get("/tasks", async (req, res) => {
-    const tasks = await TaskModel.find({});
-    res.status(200).send(tasks);
-});
-
-app.get("/tasks/:id", async (req, res) => {
-    const task = await TaskModel.findById(req.params.id);
-    if (!task) {
-        return res.status(404).send("Task not found");
-    }
-    res.status(200).send(task);
-});
-
-app.post("/tasks", async (req, res) => {
-    const task = await TaskModel.create(req.body);
-    res.status(201).send(task);
-});
-
-app.patch("/tasks/:id", async (req, res) => {
-    try {
-        const allowedUpdates = ["isCompleted"];
-        const updates = Object.keys(req.body);
-        const isValidOperation = updates.every((update) =>
-            allowedUpdates.includes(update)
-        );
-        if (!isValidOperation) {
-            return res.status(400).send({ error: "Invalid updates!" });
-        }
-        const task = await TaskModel.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            {
-                new: true,
-            }
-        );
-        res.status(200).send(task);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
-app.delete("/tasks/:id", async (req, res) => {
-    const taskId = await TaskModel.findById(req.params.id);
-    if (!taskId) {
-        return res.status(404).send("Task not found");
-    }
-
-    const task = await TaskModel.findByIdAndDelete(req.params.id);
-    res.status(200).send(task);
-});
+app.use("/tasks", router);
 
 app.listen(process.env.PORT, () =>
     console.log(`Listening on port ${process.env.PORT}!`)
