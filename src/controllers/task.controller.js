7c0034b6ec2f6321b12020d89797a1d0d4227137
luizengdev <luizengdev@gmail.com>
2024-01-16 18:53:11 -0300

    feat: move get id, post, patch end delete tasks endpoint to a controller

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -6,10 +6,68 @@ class TaskController {
         this.res = res;
     }
 
-    async getTasks() {
+    async getAll() {
         try {
             const tasks = await TaskModel.find({});
             this.res.status(200).send(tasks);
+        } catch (error) {
+            this.res.status(500).send(error.message);
+        }
+    }
+
+    async getById() {
+        try {
+            const task = await TaskModel.findById(this.req.params.id);
+            if (!task) {
+                this.res.status(404).send("Task not found");
+            }
+            this.res.status(200).send(task);
+        } catch (error) {
+            this.res.status(500).send(error.message);
+        }
+    }
+
+    async created() {
+        try {
+            const task = await TaskModel.create(this.req.body);
+            this.res.status(201).send(task);
+        } catch (error) {
+            this.res.status(500).send(error.message);
+        }
+    }
+
+    async updated() {
+        try {
+            const allowedUpdates = ["isCompleted"];
+            const updates = Object.keys(this.req.body);
+            const isValidOperation = updates.every((update) =>
+                allowedUpdates.includes(update)
+            );
+            if (!isValidOperation) {
+                return this.res.status(400).send({ error: "Invalid updates!" });
+            }
+            const task = await TaskModel.findByIdAndUpdate(
+                this.req.params.id,
+                this.req.body,
+                {
+                    new: true,
+                }
+            );
+            this.res.status(200).send(task);
+        } catch (error) {
+            this.res.status(500).send(error.message);
+        }
+    }
+
+    async deleted() {
+        try {
+            const taskId = await TaskModel.findById(this.req.params.id);
+            if (!taskId) {
+                return this.res.status(404).send("Task not found");
+            }
+
+            const task = await TaskModel.findByIdAndDelete(this.req.params.id);
+            this.res.status(200).send(task);
         } catch (error) {
             this.res.status(500).send(error.message);
         }
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -5,53 +5,23 @@ import TaskModel from "../models/task.model.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    return new TaskController(req, res).getTasks();
+    return new TaskController(req, res).getAll();
 });
 
 router.get("/:id", async (req, res) => {
-    const task = await TaskModel.findById(req.params.id);
-    if (!task) {
-        return res.status(404).send("Task not found");
-    }
-    res.status(200).send(task);
+    return new TaskController(req, res).getById();
 });
 
 router.post("/", async (req, res) => {
-    const task = await TaskModel.create(req.body);
-    res.status(201).send(task);
+    return new TaskController(req, res).created();
 });
 
 router.patch("/:id", async (req, res) => {
-    try {
-        const allowedUpdates = ["isCompleted"];
-        const updates = Object.keys(req.body);
-        const isValidOperation = updates.every((update) =>
-            allowedUpdates.includes(update)
-        );
-        if (!isValidOperation) {
-            return res.status(400).send({ error: "Invalid updates!" });
-        }
-        const task = await TaskModel.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            {
-                new: true,
-            }
-        );
-        res.status(200).send(task);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+    return new TaskController(req, res).updated();
 });
 
 router.delete("/:id", async (req, res) => {
-    const taskId = await TaskModel.findById(req.params.id);
-    if (!taskId) {
-        return res.status(404).send("Task not found");
-    }
-
-    const task = await TaskModel.findByIdAndDelete(req.params.id);
-    res.status(200).send(task);
+    return new TaskController(req, res).deleted();
 });
 
 export default router;
