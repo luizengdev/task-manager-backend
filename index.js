02ed57ab8e38a7ee050182fcf9007a9de88cffe0
luizengdev <luizengdev@gmail.com>
2024-01-18 16:56:01 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+
 import router from "./src/routes/task.routes.js";
 import connectToDatabase from "./src/database/mongoose.database.js";
 
 dotenv.config();
-const app = express();
 
+const app = express();
+app.use(cors());
 app.use(express.json());
 
 connectToDatabase();
